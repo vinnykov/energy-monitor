184d0a69d49bc888ef7afb327d8ed1d3223f1475
Igor Vinnykov <i.vinnykov@gmail.com>
2021-08-18 01:14:29 +0300

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,12 @@
+/* Energy Meter
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "sdkconfig.h"
+
+void app_main(void) {
+
+}
